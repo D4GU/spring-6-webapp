3beb376b347fd32b894f63be87be7bba712a1a4f
gugd <daniel.guggisberg@elca.ch>
2023-04-04 12:47:01 +0200

    DepInj

--- src/main/java/guru/springframework/spring6webapp/services/BookService.java
+++
@@ -1,9 +0,0 @@
-package guru.springframework.spring6webapp.services;
-
-import guru.springframework.spring6webapp.domain.Book;
-import org.springframework.stereotype.Service;
-
-
-public interface BookService {
-    Iterable<Book> findAll();
-}
